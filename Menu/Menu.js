da63fb11684d5dba6018ff21fb585f35ddf06285
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-01-30 14:53:22 -0500

    Menu.Js file completed according to given instruction

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,12 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+// Toggle the "menu--open" class on your menu refence. 
+let toggleMenu = () =>{
+menu.classList.toggle('.menu--open');
 }
-
 // Start Here: Create a reference to the ".menu" class
 const menu;
+let menu = document.querySelector('.menu-button');
 // create a reference to the ".menu-button" class
 const menuButton;
+menuButton.addEventListener('click', toggleMenu);
 // Using your menuButton reference, add a click handler that calls toggleMenu
