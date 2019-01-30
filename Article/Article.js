b86604c0d03b6f96ef7bbddea7c9497556baeb65
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-01-30 17:29:28 -0500

    Some animation added for article component

--- Article/Article.js
+++ Article/Article.js
@@ -7,13 +7,23 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerHTML = 'Expand';
+    this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle)
+    this.expandButton.addEventListener('click', () => this.expandArticle())
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.parentNode.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
+    if (this.domElement.classList.contains('article-open')) {
+      TweenMax.to(this.domElement, 1, {height: 400});
+      TweenMax.to(this.domElement, 1,{y:10, delay:0.2});
+      this.expandButton.textContent = 'Close';
+    } else {
+      TweenMax.to(this.domElement, 1,{height: 50});
+      TweenMax.to(this.domElement, 1,{x:80, delay:0.2});
+      TweenMax.to(this.domElement, 1,{y:-10, delay:0.2});
+      this.expandButton.textContent = 'Expand';
+    };
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,11 @@
 // Toggle the "menu--open" class on your menu refence. 
 let toggleMenu = () =>{
 menu.classList.toggle('.menu--open');
+if (menu.classList.contains("menu--open")) {
+  TweenMax.to(menu, 1, {left: 0})
+} else {
+  TweenMax.to(menu, 1, {left: -350})
+}
 }
 // Start Here: Create a reference to the ".menu" class
 let menu = document.querySelector('.menu');
